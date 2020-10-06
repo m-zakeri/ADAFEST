8adcf23834274cc3ee58a5442055010be68c1257
m-zakeri <m-zakeri@live.com>
2020-10-06 10:51:58 +0330

    Initial commit. Adding README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+## ADAFEST: A Data-Driven Approach For Estimating / Evaluating Software Testability
+
+### Release date
+The full version of source code will be available as soon as the relevant paper(s) are published.
---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+ADAFEST: A Data-Driven Approach to Estimating / Evaluating Software Testability
+Main file.
+The full version of source code will be available as soon as the relevant paper(s) are published.
+
+"""
+
+
+def print_welcome(name) -> None:
+    """
+    Welcome to project {ADAFEST,}
+    :param name: The name of the project
+    :return: None
+    """
+    print(f'Welcome to the project {name}.')
+
+
+# Main driver
+if __name__ == '__main__':
+    print_welcome('ADAFEST')
