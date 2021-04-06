d13f73682aecded34b4e8fa203435e56dd7a280a
m-zakeri <m-zakeri@live.com>
2021-04-06 23:46:39 +0430

    Updates metrics_api_1.py

--- metrics/metrics_api_1.py
+++ metrics/metrics_api_1.py
@@ -22,59 +22,7 @@ except ModuleNotFoundError:
     raise ModuleNotFoundError('Understand cannot import')
 
 
-class Metric(ABC):
-    def __init__(self, db=None,
-                 project_name: str = None, ):
-        self.db = db
-        self.project_name = project_name
-
-    @abstractmethod
-    def compute_metric(self, metric_name: str = None):
-        pass
-
-
-class PackageMetric(Metric):
-    def __init__(self, db=None,
-                 project_name: str = None,
-                 package_name: str = None, ):
-        super(PackageMetric, self).__init__(db=db, project_name=project_name)
-        self.package_name = package_name
-
-    def compute_metric(self, metric_name: str = None):
-        pass
-
-
-class ClassMetric(PackageMetric):
-    def __init__(self, db=None,
-                 project_name: str = None,
-                 package_name: str = None,
-                 class_name: str = None,
-                 method_name: str = None):
-        super(ClassMetric, self).__init__(db=db, project_name=project_name, package_name=package_name)
-        self.class_name = class_name
-
-    def compute_metric(self, metric_name: str = None):
-        if metric_name == 'MinCyclomatic':
-            pass
-
-
-class MethodMetric(ClassMetric):
-    def __init__(self, db=None,
-                 project_name: str = None,
-                 package_name: str = None,
-                 class_name: str = None,
-                 method_name: str = None):
-        super(MethodMetric, self).__init__(db=db,
-                                           project_name=project_name,
-                                           package_name=package_name,
-                                           class_name=class_name)
-        self.method_name = method_name
-
-    def compute_metric(self, metric_name: str = None):
-        pass
-
-
-class UnderstandUtility(object):
+class UnderstandUtility:
     """
     https://scitools.com/documents/manuals/python/understand.html
     https://scitools.com/documents/manuals/perl/#ada_entity_kinds
@@ -518,6 +466,58 @@ class UnderstandUtility(object):
     def get_entity_kind(cls, db, class_name):
         entity = db.lookup(class_name + '$', 'Type')
         return entity[0].kindname()
+
+
+class Metric(ABC):
+    def __init__(self, db=None,
+                 project_name: str = None, ):
+        self.db = db
+        self.project_name = project_name
+
+    @abstractmethod
+    def compute_metric(self, metric_name: str = None):
+        pass
+
+
+class PackageMetric(Metric):
+    def __init__(self, db=None,
+                 project_name: str = None,
+                 package_name: str = None, ):
+        super(PackageMetric, self).__init__(db=db, project_name=project_name)
+        self.package_name = package_name
+
+    def compute_metric(self, metric_name: str = None):
+        pass
+
+
+class ClassMetric(PackageMetric):
+    def __init__(self, db=None,
+                 project_name: str = None,
+                 package_name: str = None,
+                 class_name: str = None,
+                 method_name: str = None):
+        super(ClassMetric, self).__init__(db=db, project_name=project_name, package_name=package_name)
+        self.class_name = class_name
+
+    def compute_metric(self, metric_name: str = None):
+        if metric_name == 'MinCyclomatic':
+            pass
+
+
+class MethodMetric(ClassMetric):
+    def __init__(self, db=None,
+                 project_name: str = None,
+                 package_name: str = None,
+                 class_name: str = None,
+                 method_name: str = None):
+        super(MethodMetric, self).__init__(db=db,
+                                           project_name=project_name,
+                                           package_name=package_name,
+                                           class_name=class_name)
+        self.method_name = method_name
+
+    def compute_metric(self, metric_name: str = None):
+        pass
 
 
 if __name__ == '__main__':
